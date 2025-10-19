fbda5f694c76bea4b96ffd7b5643b5a60708d098
Abenezer Eshetie <abenezergoo@gmail.com>
2025-10-20 00:09:52 +0300

    feat: add typeorm to nestjs

--- flex-living/apps/backend/src/app.module.ts
+++ flex-living/apps/backend/src/app.module.ts
@@ -3,9 +3,25 @@ import { ConfigModule } from '@nestjs/config';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { ReviewModule } from './review/review.module';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { Review } from './entities/review.entity';
 
 @Module({
-  imports: [ConfigModule.forRoot({ isGlobal: true }), ReviewModule],
+  imports: [
+    ConfigModule.forRoot({ isGlobal: true }),
+    TypeOrmModule.forRoot({
+      type: 'postgres',
+      host: 'localhost',
+      port: 5432,
+      username: 'dev_user',
+      password: 'dev_password',
+      database: 'app_db',
+      entities: [Review],
+      synchronize: true, // to disable in prod
+      logging: true,
+    }),
+    ReviewModule,
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
--- flex-living/apps/backend/src/review/review.service.ts
+++ flex-living/apps/backend/src/review/review.service.ts
@@ -1,15 +1,22 @@
 import { Injectable } from '@nestjs/common';
 import { CreateReviewDto } from './dto/create-review.dto';
 import { UpdateReviewDto } from './dto/update-review.dto';
+import { InjectRepository } from '@nestjs/typeorm';
+import { Review } from '../entities/review.entity';
+import { Repository } from 'typeorm';
 
 @Injectable()
 export class ReviewService {
-  constructor() {}
+  constructor(
+    @InjectRepository(Review)
+    private reviewRepo: Repository<Review>,
+  ) {}
   create(createReviewDto: CreateReviewDto) {
     return 'This action adds a new review';
   }
 
   findAll() {
+    return this.reviewRepo.find();
     return `This action returns all review`;
   }
 
